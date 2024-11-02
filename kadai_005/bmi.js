a8e11d88cf1b949de1a9bc4e1af2985523bdea0b
yuchel222 <youyoumoremore811@gmail.com>
2024-11-02 11:11:37 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,18 @@
+// 変数の宣言
+let userName;
+
+//　値の代入
+userName = '体重';
+userNumber = 68;
+console.log(userName);
+console.log(userNumber);
+
+userName = '身長';
+userNumber = 1.7;
+console.log(userName);
+console.log(userNumber);
+
+userName = 'BMI';
+userNumber =(68 / (1.7 * 1.7) );
+console.log(userName);
+console.log(userNumber);
