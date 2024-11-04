ef056dd7acca62f92adfc290e1c79cbde2961672
yuchel222 <youyoumoremore811@gmail.com>
2024-11-04 12:49:46 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
-const birthday = new Date(2024,9,12);
- const year = birthday.getFullYear();
- const month = birthday.getMonth() + 1;  // JavaScriptの月は0から11なので1を加えます
- const day = birthday.getDate();
+const today = new Date();
+ const year = today.getFullYear();
+ const month = today.getMonth() + 1;  // JavaScriptの月は0から11なので1を加えます
+ const day = today.getDate();
 
-const birthdays = `${year}年${month}月${day}日`;
-console.log(birthdays);
+const todays = `${year}年${month}月${day}日`;
+console.log(todays);
